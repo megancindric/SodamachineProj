e5cecc8c90cd429fd78958695b21ecb0b13f2aa3
megancindric <megancindric@gmail.com>
2020-06-16 11:20:59 -0500

    Added abstract classes (coin, can) w/ child classes.  Added customer, Backpack, Machine, Wallet, Card classes

---
+++ SodaMachine/Machine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SodaMachine
+{
+    class Machine
+    {
+        //member variables
+            //Will have a register list of coins and inventory list of cans
+
+        //constructor
+
+        //member methods
+    }
+}
