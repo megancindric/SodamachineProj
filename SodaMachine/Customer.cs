6d9f750631a8e61179a0fe9ff18d82aeae90274d
megancindric <megancindric@gmail.com>
2020-06-17 11:43:10 -0500

    Added a list of coins to Customer class for payment.  Added member method to add to this list from customer's wallet

--- SodaMachine/Customer.cs
+++ SodaMachine/Customer.cs
@@ -6,5 +6,36 @@ namespace SodaMachine
 {
     class Customer
     {
+        //member variables
+        public Wallet wallet;
+        public Backpack backpack;
+        public List<Coin> payment;
+            //Will have a Wallet (with money) and Backpack(with a list of Cans)
+
+        //constructor
+        public Customer()
+        {
+            wallet = new Wallet();
+            backpack = new Backpack();
+            payment = new List<Coin>();
+
+        }
+
+        //member methods
+        public void AddToPayment(Coin coin)
+        {
+            if (wallet.coins.Contains(coin))
+            {
+                wallet.coins.Remove(coin);
+                payment.Add(coin);
+            }
+            else
+            {
+                Console.WriteLine($"Whoops!  Looks like you're out of {coin.Name}.  Try again!");
+            }
+        }
+
+
+       
     }
 }
