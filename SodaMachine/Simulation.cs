c44b7cd97476cb34484d2f48ab0893ed471948ef
megancindric <megancindric@gmail.com>
2020-06-17 12:19:28 -0500

    Added methods for receiving and returning payment to Simulation class

--- SodaMachine/Simulation.cs
+++ SodaMachine/Simulation.cs
@@ -6,5 +6,63 @@ namespace SodaMachine
 {
     class Simulation
     {
+        //member variables
+        //Will have a SodaMachine and a Customer object
+        public Machine sodaMachine;
+        public Customer customer;
+
+        //constructor
+        public Simulation()
+        {
+            sodaMachine = new Machine();
+            customer = new Customer();
+        }
+
+        //member methods
+        public void InsertPayment(List<Coin> customerPayment)
+        {
+                sodaMachine.receivedPayment = customerPayment;
+        }
+
+        public void ReturnPayment(List<Coin> receivedPayment)
+        {
+            customer.wallet.coins = receivedPayment;
+        }
+        public void InsufficientFunds()
+        {
+            Interface.InsufficientMoney();
+            ReturnPayment(sodaMachine.receivedPayment);
+        }
+
+        public void InsufficientInventory()
+        {
+            Interface.InsufficientInventory();
+            ReturnPayment(sodaMachine.receivedPayment);
+        }
+        public double ComputeTotalPayment(List<Coin> coinList)
+        {
+            double totalPayment = 0;
+            foreach (Coin coin in coinList)
+            {
+                totalPayment += coin.Value;
+            }
+            return totalPayment;
+        }
+
+        public void AssessPayment(double totalPayment, Can colaSelection)
+        {
+
+        }
+
+        public void OverPayment()
+        {
+
+        }
+
+        public void UnderPayment()
+        {
+            Interface.InsufficientMoney();
+            ReturnPayment(sodaMachine.receivedPayment);
+        }
     }
 }
